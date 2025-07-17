c0523533d0454af396af8b94ab0da5900046f80a
Rinzin Dolma <rsamrab7@gmail.com>
2025-07-17 16:01:51 -0400

    Added the dropdown to unwind but struggling to make them clickable

--- front-end/src/pages/unwind.jsx
+++ front-end/src/pages/unwind.jsx
@@ -1,12 +1,36 @@
 import NavBar from "../components/NavBar";
+import React from "react";
+import { useState } from "react";
 function Unwind() {
-    return ( 
-        <div className="unwind"> 
-        <NavBar />
-        <h3>WELCOME TO UNWIND</h3>
-        
-        </div>
-     );
+  const [isOpen, setIsOpen]=useState(false);
+  const [selectedItem, setSelectedItem]=useState("How are you feeling today?")
+  const toggleDropDown =()=>{
+    setIsOpen(!isOpen);
+  };
+  const handleItemClick=(item=>{
+    setSelectedItem(item);
+    setIsOpen(false);
+  })
+  return (
+    <div className="unwind">
+      <NavBar />
+      <h3>WELCOME TO UNWIND</h3>
+      <div className="dropdown-container">
+        <button onClick={toggleDropDown} className="dropdown-toggle">
+          {selectedItem}
+        </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          <li onClick={()=>handleItemClick('Option 1')}>Happy</li>
+          <li onClick={()=>handleItemClick('Option 2')}>Sad</li>
+          <li onClick={()=>handleItemClick('Option 3')}>Guilty</li>
+          <li onClick={()=>handleItemClick('Option 4')}>Love</li>
+          <li onClick={()=>handleItemClick('Option 5')}>Loneliness</li>
+        </ul>
+      )}
+      </div>
+    </div>
+  );
 }
 
 export default Unwind;
