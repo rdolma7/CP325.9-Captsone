d91aad753a890dd965bd2f3cfddfd3f00f8eb330
Rinzin Dolma <rsamrab7@gmail.com>
2025-07-17 14:55:43 -0400

    Added ternary logic so the button selected only shows that specific form

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import React from "react";
 
 function Login() {
-  const [login, setLogin] = useState("");
-  const [signup, setSignup] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   //   Login States
   const [loginEmail, setLoginEmail] = useState("");
@@ -23,80 +21,74 @@ function Login() {
   return (
     <>
       <div>
-        <div>
-          <button
-            onClick={() => {
-              setSignup(false);
-            }}
-          >
-            Login
-          </button>
-          <button
-            onClick={() => {
-              setSignup(true);
-            }}
-          >
-            Signup
-          </button>
+        <button
+          onClick={() => {
+            setSignup(false);
+          }}
+        >
+          Login
+        </button>
+        <button
+          onClick={() => {
+            setSignup(true);
+          }}
+        >
+          Signup
+        </button>
+      </div>
+      <div className={`form_Selected ${isLoggedIn ? "form_Selected2": ""}`}> {/* Ternary */}
+        <div className="loginForm">
+          <h1>Login</h1>
+          <label className="label">Email: </label>
+          <input
+            value={loginEmail}
+            onChange={(event) => setLoginEmail(event.target.value)}
+            className="email"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <br />
+          <label className="label">Password: </label>
+          <input
+            value={loginPassword}
+            onChange={(event) => setLoginPassword(event.target.value)}
+            className="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <br />
+          {isLoginValid && <button>Login</button>}
         </div>
-        <div className={`form_Selected ${signup ? "form_Selected2" : ""}`}>
-          {" "}
-          {/* Ternary */}
-          <div className="loginForm">
-            <h1>Login</h1>
-            <label className="label">Email: </label>
-            <input
-              value={loginEmail}
-              onChange={(event) => setLoginEmail(event.target.value)}
-              className="email"
-              type="email"
-              placeholder="Enter your email"
-            />
-            <br />
-            <label className="label">Password: </label>
-            <input
-              value={loginPassword}
-              onChange={(event) => setLoginPassword(event.target.value)}
-              className="password"
-              type="password"
-              placeholder="Enter your password"
-            />
-            <br />
-            {isLoginValid && <button>Login</button> }
-            
-          </div>
-          <div className="signUpForm">
-            <h1>Signup</h1>
-            <label className="label">Full Name: </label>
-            <input
-              value={signupFullName}
-              onChange={(event) => setSignupFullName(event.target.value)}
-              type="text"
-              placeholder="Enter your name"
-              className="name"
-            />
-            <br />
-            Email :
-            <input
-              value={signupEmail}
-              onChange={(event) => setSignupEmail(event.target.value)}
-              type="email"
-              placeholder="Enter your email"
-              className="email"
-            />
-            <br />
-            Password:{" "}
-            <input
-              value={signupPassword}
-              onChange={(event) => setSignupPassword(event.target.value)}
-              type="password"
-              placeholder="Enter your password"
-              className="password"
-            />
-            <br />
-        
-            {isSignupValid && <button>Signup</button> }
-          </div>
+        <div className="signUpForm">
+          <h1>Signup</h1>
+          <label className="label">Full Name: </label>
+          <input
+            value={signupFullName}
+            onChange={(event) => setSignupFullName(event.target.value)}
+            type="text"
+            placeholder="Enter your name"
+            className="name"
+          />
+          <br />
+          Email:
+          <input
+            value={signupEmail}
+            onChange={(event) => setSignupEmail(event.target.value)}
+            type="email"
+            placeholder="Enter your email"
+            className="email"
+          />
+          <br />
+          Password:{" "}
+          <input
+            value={signupPassword}
+            onChange={(event) => setSignupPassword(event.target.value)}
+            type="password"
+            placeholder="Enter your password"
+            className="password"
+          />
+          <br />
+          {isSignupValid && <button>Signup</button>}
         </div>
       </div>
     </>
