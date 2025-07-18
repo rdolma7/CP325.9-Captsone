c31ee70e637de149c23e3329faddaff34bba3675
Rinzin Dolma <rsamrab7@gmail.com>
2025-07-18 11:13:21 -0400

    Added the functionality to the login/signup page

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   //   Login States
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
@@ -16,80 +16,81 @@ function Login() {
     signupFullName.trim() !== "" &&
     signupPassword.trim() !== "" &&
     signupEmail.trim() !== "";
+  //Navigate Hook
+  const navigate = useNavigate();
 
-  const handleClick = () => {};
+  const [isLoginSelected, setIsLoginSelected] = useState(true);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    navigate("/unwind");
+  };
   return (
     <>
       <div>
         <button
           onClick={() => {
-            setSignup(false);
+            setIsLoginSelected(true);
           }}
         >
           Login
         </button>
         <button
           onClick={() => {
-            setSignup(true);
+            setIsLoginSelected(false);
           }}
         >
           Signup
         </button>
       </div>
-      <div >
-        <div className={`loginForm ${isLoggedIn ? "loginForm2": ""}`}>
-          <h1>Login</h1>
-          <label className="label">Email: </label>
-          <input
-            value={loginEmail}
-            onChange={(event) => setLoginEmail(event.target.value)}
-            className="email"
-            type="email"
-            placeholder="Enter your email"
-          />
-          <br />
-          <label className="label">Password: </label>
-          <input
-            value={loginPassword}
-            onChange={(event) => setLoginPassword(event.target.value)}
-            className="password"
-            type="password"
-            placeholder="Enter your password"
-          />
-          <br />
-          {isLoginValid && <button>Login</button>}
-        </div>
-        <div className="signUpForm">
-          <h1>Signup</h1>
-          <label className="label">Full Name: </label>
-          <input
-            value={signupFullName}
-            onChange={(event) => setSignupFullName(event.target.value)}
-            type="text"
-            placeholder="Enter your name"
-            className="name"
-          />
-          <br />
-          Email:
-          <input
-            value={signupEmail}
-            onChange={(event) => setSignupEmail(event.target.value)}
-            type="email"
-            placeholder="Enter your email"
-            className="email"
-          />
-          <br />
-          Password:{" "}
-          <input
-            value={signupPassword}
-            onChange={(event) => setSignupPassword(event.target.value)}
-            type="password"
-            placeholder="Enter your password"
-            className="password"
-          />
-          <br />
-          {isSignupValid && <button>Signup</button>}
-        </div>
+      <div className="userForm">
+        {isLoginSelected && (
+          <form onSubmit={handleClick} className="loginForm">
+            <h1>Login</h1>
+            <label className="label">Email: </label>
+            <input
+              className="email"
+              type="email"
+              required
+              placeholder="Enter your email"
+            />
+            <br />
+            <label className="label">Password: </label>
+            <input
+              className="password"
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
+            <br />
+            <button>Login</button>
+          </form>
+        )}
+        {!isLoginSelected && (
+          <form onSubmit={handleClick} className="signUpForm">
+            <h1>Signup</h1>
+            <label className="label">Full Name: </label>
+            <input required type="text" placeholder="Enter your name" className="name" />
+            <br />
+            Email:
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="email"
+              required
+            />
+            <br />
+            Password:{" "}
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="password"
+              required
+            />
+            <br />
+            <button>Signup</button>
+          </form>
+        )}
       </div>
     </>
   );
