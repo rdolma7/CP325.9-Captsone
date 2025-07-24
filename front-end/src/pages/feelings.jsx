bf68ed7ea28784f0e6e1a0976f1d3bece0e41af8
Rinzin Dolma <rsamrab7@gmail.com>
2025-07-24 14:25:58 -0400

    css changes

--- front-end/src/pages/feelings.jsx
+++ front-end/src/pages/feelings.jsx
@@ -6,7 +6,7 @@ function Feelings() {
   const [selectedItem, setSelectedItem] = useState("");
   const [moodData, setMoodData] = useState(null);
 
-  const moodMap = {
+  const moodMap = { /*moodMap  */
     sad: {
       url: "https://www.youtube.com/embed/RUP-S0WhP6w",
       text: "Sadness, a common human emotion, can be profound and isolating, but also a source of strength and empathy.",
